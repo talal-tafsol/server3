f5b7343a27f09fa5f593e8e2dee2be156e394ab5
talal-tafsol <talalafzaal.tafsol@gmail.com>
2024-02-20 19:02:34 +0500

    init

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Hello from Server 3",
+  });
+});
+
+app.get("/test", (req, res) => {
+  res.status(200).json({
+    message: "Hello from Test Server 3",
+  });
+});
+
+app.listen(PORT, () => console.log(`Server 3 is running on port: ${PORT}`));
